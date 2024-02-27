28b879779d9b15e0e710f28873afc68f4057f85f
dimausac <usachev2012@gmail.com>
2024-02-27 14:46:52 +0400

    roles

--- SCRIPTS/roles.sql
+++ SCRIPTS/roles.sql
@@ -23,7 +23,33 @@ select grantee, privilege_type
 from information_schema.role_table_grants
 where table_name = 'orders';
 
+--  уровень таблиц
+
+
 grant select, insert, update, delete on table public.orders, order_details, products to sales_staff;
 
 grant select, insert, update, delete, references, trigger on ALL TABLES IN SCHEMA public
     to northwind_admins;
+
+-- уровень колонок
+
+grant select (first_name, last_name, photo_path, photo) on employees to sales_staff;
+
+revoke select on employees from sales_staff;
+
+revoke all privileges on orders from sales_staff;
+revoke all on database test from sales_staff;
+revoke all on schema public from sales_staff;
+
+select *
+from pg_roles;
+
+SELECT
+	10/2 AS "деление",
+	35*3 AS "умножение",
+	38-4 AS "вычитание",
+	45+12 AS "сложение",
+	13/2 AS "деление целых чисел",
+	13.0/2 AS "правильное деление",
+	13%2 AS "целочисленное деление",
+	25%7 AS "целочисленное деление";
